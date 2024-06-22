6354c3fb8297856438e418851a2e271170226102
Xie Yong <1620469296@qq.com>
2024-06-22 21:46:15 +0800

    add gradio 4.36.1

--- app_new.py
+++ app_new.py
@@ -316,7 +316,6 @@ if __name__ == "__main__":
 
     # 环境命令
     # os.chdir('/home/xlab-app-center/')
-    os.system('pip install --upgrade gradio==4.36.1')
     
     LLM_openxlab_path = "shenfeilang/Honor-of-Kings_RolePlay"
     lively_openxlab_path = "YongXie66/DaJi_RolePlay"
